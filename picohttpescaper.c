bc8350e9b8a1a6cb96cc1e07e669d61a510484b0
moznion <moznion@gmail.com>
2015-09-05 02:02:11 +0900

    Simplify

--- picohttpescaper.c
+++ picohttpescaper.c
@@ -25,69 +25,68 @@ char *phe_escape_html(const char *p, size_t size) {
 
     __m128i v;
     do {
-        v = _mm_loadu_si128((const __m128i*)p);
-        if (!_mm_cmpestra(ranges, RANGE_SIZE, v, size, 4)) {
-            if (_mm_cmpestrc(ranges, RANGE_SIZE, v, size, 4)) {
-                cursor = _mm_cmpestri(ranges, RANGE_SIZE, v, size, 4);
-                memcpy(&buf[buf_index], p, cursor);
-                buf_index += cursor;
-                const char c = p[cursor];
-                switch (c) {
-                    case '&':
-                        memcpy(&buf[buf_index], "&amp;", 5);
-                        buf_index += 5;
-                        break;
-                    case '>':
-                        memcpy(&buf[buf_index], "&gt;", 4);
-                        buf_index += 4;
-                        break;
-                    case '<':
-                        memcpy(&buf[buf_index], "&lt;", 4);
-                        buf_index += 4;
-                        break;
-                    case '"':
-                        memcpy(&buf[buf_index], "&quot;", 6);
-                        buf_index += 6;
-                        break;
-                    case '\'':
-                        memcpy(&buf[buf_index], "&#39;", 5);
-                        buf_index += 5;
-                        break;
-                    case '`':
-                        // For IE. IE interprets back-quote as valid quoting characters
-                        // ref: https://rt.cpan.org/Public/Bug/Display.html?id=84971
-                        memcpy(&buf[buf_index], "&#96;", 5);
-                        buf_index += 5;
-                        break;
-                    case '{':
-                        // For javascript templates (e.g. AngularJS and such javascript frameworks)
-                        // ref: https://github.com/angular/angular.js/issues/5601
-                        memcpy(&buf[buf_index], "&#123;", 6);
-                        buf_index += 6;
-                        break;
-                    case '}':
-                        // For javascript templates (e.g. AngularJS and such javascript frameworks)
-                        // ref: https://github.com/angular/angular.js/issues/5601
-                        memcpy(&buf[buf_index], "&#125;", 6);
-                        buf_index += 6;
-                        break;
-                    default:
-                        memcpy(&buf[buf_index], &c, 1);
-                        buf_index += 1;
-
-                }
-
-                const int next = cursor + 1;
-                p += next;
-                size -= next;
-                continue;
+        v = _mm_loadu_si128((const __m128i*) p);
+        cursor = _mm_cmpestri(ranges, RANGE_SIZE, v, 16, _SIDD_LEAST_SIGNIFICANT | _SIDD_CMP_RANGES | _SIDD_UBYTE_OPS);
+        if (cursor != 16) {
+            memcpy(&buf[buf_index], p, cursor);
+            buf_index += cursor;
+            const char c = p[cursor];
+            switch (c) {
+                case '&':
+                    memcpy(&buf[buf_index], "&amp;", 5);
+                    buf_index += 5;
+                    break;
+                case '>':
+                    memcpy(&buf[buf_index], "&gt;", 4);
+                    buf_index += 4;
+                    break;
+                case '<':
+                    memcpy(&buf[buf_index], "&lt;", 4);
+                    buf_index += 4;
+                    break;
+                case '"':
+                    memcpy(&buf[buf_index], "&quot;", 6);
+                    buf_index += 6;
+                    break;
+                case '\'':
+                    memcpy(&buf[buf_index], "&#39;", 5);
+                    buf_index += 5;
+                    break;
+                case '`':
+                    // For IE. IE interprets back-quote as valid quoting characters
+                    // ref: https://rt.cpan.org/Public/Bug/Display.html?id=84971
+                    memcpy(&buf[buf_index], "&#96;", 5);
+                    buf_index += 5;
+                    break;
+                case '{':
+                    // For javascript templates (e.g. AngularJS and such javascript frameworks)
+                    // ref: https://github.com/angular/angular.js/issues/5601
+                    memcpy(&buf[buf_index], "&#123;", 6);
+                    buf_index += 6;
+                    break;
+                case '}':
+                    // For javascript templates (e.g. AngularJS and such javascript frameworks)
+                    // ref: https://github.com/angular/angular.js/issues/5601
+                    memcpy(&buf[buf_index], "&#125;", 6);
+                    buf_index += 6;
+                    break;
+                default:
+                    memcpy(&buf[buf_index], &c, 1);
+                    buf_index += 1;
             }
+
+            const int next = cursor + 1;
+            p += next;
+            size -= next;
+            continue;
         }
+
         memcpy(&buf[buf_index], p, 16);
         buf_index += 16;
         p += 16;
         size -= 16;
-    } while((int)size > 0);
+    } while((int) size > 0);
 
     return buf;
 }
+
---
+++ picohttpescaper.h
@@ -0,0 +1,14 @@
+#ifndef picohttpparser_h
+#define picohttpparser_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *phe_escape_html(const char *p, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
