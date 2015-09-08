f631857886ff322e16469567902bb6ddc97355f7
moznion <moznion@gmail.com>
2015-09-09 00:47:14 +0900

    Add test cases

--- test.c
+++ test.c
@@ -9,9 +9,11 @@ void ok_escape (const char *src, const char *expected);
 
 void chars (void);
 void mixed (void);
+void basic (void);
 
 int main (void) {
     subtest("chars", chars);
+    subtest("basic", basic);
     subtest("mixed", mixed);
     return done_testing();
 }
@@ -46,5 +48,13 @@ void mixed (void) {
             "'`><script type=\"text/javascript\">{{alert(1)}}</script>&",
             "&#39;&#96;&gt;&lt;script type=&quot;text/javascript&quot;&gt;&#123;&#123;alert(1)&#125;&#125;&lt;/script&gt;&amp;"
             );
+}
+
+void basic (void) {
+    ok_escape("<^o^>", "&lt;^o^&gt;");
+    ok_escape("'\"", "&#39;&quot;");
+    ok_escape("~\0>", "~\0&gt;");
+    ok_escape("`~", "&#96;~");
+    ok_escape("aあaあaあ{aあaあaあ}aあaあaあ", "aあaあaあ&#123;aあaあaあ&#125;aあaあaあ");
 }
 
