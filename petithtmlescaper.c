b136aebe7a4cdfb7e6cc49a6a6640613b2d299e6
moznion <moznion@gmail.com>
2015-09-12 17:04:04 +0900

    Support environment which is SSE4.2 missing

--- petithtmlescaper.c
+++ petithtmlescaper.c
@@ -11,7 +11,14 @@
 #define RANGES "\">``{}"
 #define RANGE_SIZE 6
 
+static inline void _phe_escape_html(char *dst, size_t dst_size, const char *input, size_t input_size);
+
 void phe_escape_html(char *dst, size_t dst_size, const char *input, size_t input_size) {
+    _phe_escape_html(dst, dst_size, input, input_size);
+}
+
+#if __SSE4_2__
+static inline void _phe_escape_html(char *dst, size_t dst_size, const char *input, size_t input_size) {
     const __m128i ranges = _mm_loadu_si128((const __m128i*) RANGES);
 
     int cursor = 0;
@@ -95,4 +102,54 @@ void phe_escape_html(char *dst, size_t dst_size, const char *input, size_t input
 
     *dst++ = *"\0";
 }
-
+#else
+static inline void _phe_escape_html(char *dst, size_t dst_size, const char *input, size_t input_size) {
+    int begin = 0;
+    for (int i = 0; i < input_size; i++) {
+        const char c = *(input++);
+        switch (c) {
+            case '&':
+                memcpy(&dst[begin], "&amp;", 5);
+                begin += 5;
+                break;
+            case '>':
+                memcpy(&dst[begin], "&gt;", 4);
+                begin += 4;
+                break;
+            case '<':
+                memcpy(&dst[begin], "&lt;", 4);
+                begin += 4;
+                break;
+            case '"':
+                memcpy(&dst[begin], "&quot;", 6);
+                begin += 6;
+                break;
+            case '\'':
+                memcpy(&dst[begin], "&#39;", 5);
+                begin += 5;
+                break;
+            case '`':
+                // For IE. IE interprets back-quote as valid quoting characters
+                // ref: https://rt.cpan.org/Public/Bug/Display.html?id=84971
+                memcpy(&dst[begin], "&#96;", 5);
+                begin += 5;
+                break;
+            case '{':
+                // For javascript templates (e.g. AngularJS and such javascript frameworks)
+                // ref: https://github.com/angular/angular.js/issues/5601
+                memcpy(&dst[begin], "&#123;", 6);
+                begin += 6;
+                break;
+            case '}':
+                // For javascript templates (e.g. AngularJS and such javascript frameworks)
+                // ref: https://github.com/angular/angular.js/issues/5601
+                memcpy(&dst[begin], "&#125;", 6);
+                begin += 6;
+                break;
+            default:
+                memcpy(&dst[begin], &c, 1);
+                begin += 1;
+        }
+    }
+}
+#endif
